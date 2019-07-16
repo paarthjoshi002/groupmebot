b0c7ca408073fc13439f74994a531f12a07386a4
paarthjoshi002 <44733838+paarthjoshi002@users.noreply.github.com>
2019-07-16 18:06:47 -0500

    adding logic before writing status code

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
+  var endingString = this.res.url;
   this.res.writeHead(200);
-  var endingString = this.res.ok;
   this.res.end(String(endingString));
 }
