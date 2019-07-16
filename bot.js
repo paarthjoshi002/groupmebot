4b7b0b8d1816588ff34a78f9b4bf52770aec9ac2
paarthjoshi002 <44733838+paarthjoshi002@users.noreply.github.com>
2019-07-16 15:28:06 -0500

    trying /i for case sensitivity

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = new RegExp('/bible*/', 'i')   // /bible*/;
+      botRegex = /bible*/i;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
