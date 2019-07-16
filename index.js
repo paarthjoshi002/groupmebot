ebabc885ad77331e8b832d0650d6c8216240290c
paarthjoshi002 <44733838+paarthjoshi002@users.noreply.github.com>
2019-07-16 18:08:37 -0500

    reverting

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ port = Number(process.env.PORT || 5000);
 server.listen(port);
 
 function ping() {
-  var endingString = this.res.url;
   this.res.writeHead(200);
-  this.res.end(String(endingString));
+  this.res.end("application finished");
 }
