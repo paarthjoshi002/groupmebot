62780415d9e848c5e180f4b85a183aaf16e578b1
paarthjoshi002 <44733838+paarthjoshi002@users.noreply.github.com>
2020-09-13 17:16:02 -0500

    remove old comments

--- bot.js
+++ bot.js
@@ -43,12 +43,6 @@ function postMessage() {
     path: '/v3/groups/42071764/members/353065980/remove?token=36633e408a260137b88a62dd589ed495',
     method: 'POST'
   };
-  
-  /*options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/groups/42071764/members/8c976aac-885c-465e-9c06-2c70b1f1f570/remove?token=36633e408a260137b88a62dd589ed495',
-    method: 'POST'
-  };*/
 
   /*body = {
     "bot_id" : botID,
